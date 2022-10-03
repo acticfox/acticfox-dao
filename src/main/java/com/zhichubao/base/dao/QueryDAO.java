b035676aafe953fdd5787d3f1ada19fc31f5c024
孔凡勇 <kongfanyong@zhichubao.com>
2022-10-03 11:58:52 +0800

    init project

---
+++ src/main/java/com/zhichubao/base/dao/QueryDAO.java
@@ -0,0 +1,133 @@
+/*
+ * $Id: QueryDAO.java 252 2013-05-30 03:23:10Z fanyong.kfy $
+ *
+ * Copyright (c) 2013 zhichubao.com. All Rights Reserved.
+ */
+
+package com.zhichubao.base.dao;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * QueryDAO接口。
+ * 
+ * 用于执行查询类SQL的DAO接口。
+ * 
+ * 
+ */
+public interface QueryDAO {
+
+    /**
+     * 执行SQL并将结果转换为指定类型返回。
+     * 
+     * @param <E> 返回值类型
+     * @param sqlID 执行的SQLID
+     * @param bindParams SQL绑定参数
+     * @param clazz 返回值类
+     * @return SQL执行结果
+     */
+    <E> E executeForObject(String sqlID, Object bindParams, Class<E> clazz);
+
+    /**
+     * 执行SQL并将结果转换为Map返回。
+     * 
+     * @param sqlID 执行的SQLID
+     * @param bindParams SQL绑定参数
+     * @return SQL执行结果
+     */
+    Map<String, Object> executeForMap(String sqlID, Object bindParams);
+
+    /**
+     * 执行SQL并将结果转换为指定类型数组返回。
+     * 
+     * @param <E> 返回值类型
+     * @param sqlID 执行的SQLID
+     * @param bindParams SQL绑定参数
+     * @param clazz 返回值类
+     * @return SQL执行结果
+     */
+    <E> E[] executeForObjectArray(String sqlID,
+            Object bindParams, Class<E> clazz);
+
+    /**
+     * 执行SQL并将结果转换为Map数组返回。
+     * 
+     * @param sqlID 执行的SQLID
+     * @param bindParams SQL绑定参数
+     * @return SQL执行结果
+     */
+    Map<String, Object>[] executeForMapArray(String sqlID, Object bindParams);
+
+    /**
+     * 执行SQL并将指定的结果集转换为指定类型数组返回。
+     * 
+     * @param <E> 返回值类型
+     * @param sqlID 执行的SQLID
+     * @param bindParams SQL绑定参数
+     * @param clazz 返回值类
+     * @param beginIndex 取数据开始Index
+     * @param maxCount 取数据个数
+     * @return SQL执行结果
+     */
+    <E> E[] executeForObjectArray(String sqlID,
+            Object bindParams, Class<E> clazz, int beginIndex, int maxCount);
+
+    /**
+     * 执行SQL并将指定的结果集转换为Map数组返回。
+     * 
+     * @param sqlID 执行的SQLID
+     * @param bindParams SQL绑定参数
+     * @param beginIndex 取数据开始Index
+     * @param maxCount 取数据个数
+     * @return SQL执行结果
+     */
+    Map<String, Object>[] executeForMapArray(String sqlID,
+            Object bindParams, int beginIndex, int maxCount);
+
+    /**
+     * 执行SQL并将结果转换为指定类型列表返回。
+     * 
+     * @param <E> 返回值类型
+     * @param sqlID 执行的SQLID
+     * @param bindParams SQL绑定参数
+     * @return SQL执行结果
+     */
+    <E> List<E> executeForObjectList(String sqlID,
+            Object bindParams);
+
+    /**
+     * 执行SQL并将结果转换为Map列表返回。
+     * 
+     * @param sqlID 执行的SQLID
+     * @param bindParams SQL绑定参数
+     * @return SQL执行结果
+     */
+    List<Map<String, Object>> executeForMapList(String sqlID,
+            Object bindParams);
+
+    /**
+     * 执行SQL并将指定的结果集转换为指定类型列表返回。
+     * 
+     * @param <E> 返回值类型
+     * @param sqlID 执行的SQLID
+     * @param bindParams SQL绑定参数
+     * @param beginIndex 取数据开始Index
+     * @param maxCount 取数据个数
+     * @return SQL执行结果
+     */
+    <E> List<E> executeForObjectList(String sqlID,
+            Object bindParams, int beginIndex, int maxCount);
+
+    /**
+     * 执行SQL并将指定的结果集转换为Map列表返回。
+     * 
+     * @param sqlID 执行的SQLID
+     * @param bindParams SQL绑定参数
+     * @param beginIndex 取数据开始Index
+     * @param maxCount 取数据个数
+     * @return SQL执行结果
+     */
+    List<Map<String, Object>> executeForMapList(String sqlID,
+            Object bindParams, int beginIndex, int maxCount);
+}
---
+++ src/main/java/com/zhichubao/base/dao/UpdateDAO.java
@@ -0,0 +1,48 @@
+/*
+ * $Id: UpdateDAO.java 2688 2013-10-25 07:11:10Z fanyong.kfy $
+ *
+ * Copyright (c) 2013 zhichubao.com. All Rights Reserved.
+ */
+
+package com.zhichubao.base.dao;
+
+import java.util.List;
+
+/**
+ * UpdateDAO接口。
+ * 
+ * 用于执行更新类SQL的DAO接口。
+ * 
+ * 
+ */
+public interface UpdateDAO {
+
+    /**
+     * 执行SQL并返回执行结果个数。
+     * 执行的SQL为insert, update, delete三种。
+     * 
+     * @param sqlID 执行的SQLID
+     * @param bindParams SQL绑定参数
+     * @return SQL执行结果个数
+     */
+    int execute(String sqlID, Object bindParams);
+    
+    /**
+     * 执行SQL并返回执行结果个数,只允许更新一条记录。
+     * 执行的SQL为insert, update, delete三种。
+     * 
+     * @param sqlID 执行的SQLID
+     * @param bindParams SQL绑定参数
+     * @return SQL执行结果个数
+     */
+    int executeUnique(String sqlID, Object bindParams);
+
+    /**
+     * 批量更新处理<br/>
+     * 执行参数{@link SqlHolder}列表中的全部SQL。
+     * 
+     * @param sqlHolders 包含SQLID、SQL参数的SqlHolder实例列表
+     * @return SQL执行结果个数
+     */
+    int executeBatch(List<SqlHolder> sqlHolders);
+}
